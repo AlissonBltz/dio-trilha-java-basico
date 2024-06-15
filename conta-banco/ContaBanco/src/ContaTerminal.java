ba04b6e07be4bee0923f32ff3d0d81add8a276b6
AlissonBltz <alissonbltz@gmail.com>
2024-06-15 10:07:19 -0300

    Commit 15/05/24

---
+++ conta-banco/ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String texto;
+        String nomeCliente;
+        double saldo;
+        System.out.println("Por favor digite o número da agência: ");
+        int numero = entrada.nextInt();
+
+
+        entrada.close();
+
+        
+    }
+
+}
